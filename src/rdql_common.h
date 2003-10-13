3c246fe8bf3aacb78e9d67b1328692b99c25123c
Dave Beckett <dave@dajobe.org>
2003-10-13 21:34:58 +0000

    RDQL lexer/parser shared internals

---
+++ src/rdql_common.h
@@ -0,0 +1,48 @@
+/* -*- Mode: c; c-basic-offset: 2 -*-
+ *
+ * rdql_common.h - RDQL lexer/parser shared internals
+ *
+ * $Id$
+ *
+ * Copyright (C) 2003 David Beckett - http://purl.org/net/dajobe/
+ * Institute for Learning and Research Technology - http://www.ilrt.org/
+ * University of Bristol - http://www.bristol.ac.uk/
+ * 
+ * This package is Free Software or Open Source available under the
+ * following licenses (these are alternatives):
+ *   1. GNU Lesser General Public License (LGPL)
+ *   2. GNU General Public License (GPL)
+ *   3. Mozilla Public License (MPL)
+ * 
+ * See LICENSE.html or LICENSE.txt at the top of this package for the
+ * full license terms.
+ * 
+ */
+
+#ifndef RDQL_COMMON_H
+#define RDQL_COMMON_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+struct rdql_parser_s {
+  rasqal_query *query;
+  
+  /* for lexer to store result in */
+  YYSTYPE lval;
+
+  /* STATIC lexer */
+  yyscan_t scanner;
+
+  int warnings;
+  int errors;
+};
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
