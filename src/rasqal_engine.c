2ce899d93e140b8c89442e5b02d764453cebe4f6
Dave Beckett <dave@dajobe.org>
2004-04-13 21:58:26 +0000

    (rasqal_engine_expand_qname): Expand literal datatype qnames in literals.

--- src/rasqal_engine.c
+++ src/rasqal_engine.c
@@ -207,18 +207,35 @@ static int
 rasqal_engine_expand_qname(void *user_data, rasqal_expression *e) {
   rasqal_query *rq=(rasqal_query *)user_data;
 
-  if(e->op == RASQAL_EXPR_LITERAL &&
-     e->literal->type == RASQAL_LITERAL_QNAME) {
+  if(e->op == RASQAL_EXPR_LITERAL) {
     rasqal_literal *l=e->literal;
-    raptor_uri *uri=raptor_qname_string_to_uri(rq->namespaces,
-                                               l->value.string, 
-                                               strlen(l->value.string),
-                                               rasqal_query_simple_error, rq);
-    if(!uri)
-      return 1;
-    RASQAL_FREE(cstring, l->value.string);
-    l->type=RASQAL_LITERAL_URI;
-    l->value.uri=uri; /* uri field is unioned with string field */
+    if(l->type == RASQAL_LITERAL_QNAME) {
+      /* expand a literal qname */
+      raptor_uri *uri=raptor_qname_string_to_uri(rq->namespaces,
+                                                 l->value.string, 
+                                                 strlen(l->value.string),
+                                                 rasqal_query_simple_error, rq);
+      if(!uri)
+        return 1;
+      RASQAL_FREE(cstring, l->value.string);
+      l->type=RASQAL_LITERAL_URI;
+      l->value.uri=uri; /* uri field is unioned with string field */
+    } else if (l->type == RASQAL_LITERAL_STRING) {
+      raptor_uri *uri;
+
+      if(l->flags) {
+        /* expand a literal string datatype qname */
+        uri=raptor_qname_string_to_uri(rq->namespaces,
+                                       l->flags, 
+                                       strlen(l->flags),
+                                       rasqal_query_simple_error, rq);
+        if(!uri)
+          return 1;
+        l->datatype=uri;
+        RASQAL_FREE(cstring, l->flags);
+        l->flags=NULL;
+      }
+    }
   }
   return 0;
 }
