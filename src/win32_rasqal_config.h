bde14192e26d84f659bf62e0c49d1bad51086ac9
Dave Beckett <dave@dajobe.org>
2005-02-06 19:10:55 +0000

    Bumped version to 0.9.7

--- src/win32_rasqal_config.h
+++ src/win32_rasqal_config.h
@@ -64,7 +64,7 @@ extern "C" {
  */
 
 /* Release version as a decimal */
-#define RASQAL_VERSION_DECIMAL 906
+#define RASQAL_VERSION_DECIMAL 907
 
 /* Major version number */
 #define RASQAL_VERSION_MAJOR 0
@@ -73,10 +73,10 @@ extern "C" {
 #define RASQAL_VERSION_MINOR 9
 
 /* Release version number */
-#define RASQAL_VERSION_RELEASE 6
+#define RASQAL_VERSION_RELEASE 7
 
 /* Version number of package */
-#define VERSION "0.9.6"
+#define VERSION "0.9.7"
 
 #include <windows.h>
 #include <io.h>
