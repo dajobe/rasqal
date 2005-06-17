3488e9443f45489007bac35cc47ec8c0652d3bc8
Dave Beckett <dave@dajobe.org>
2005-06-17 10:49:42 +0000

    Update rasqal_literal_string_to_native prototype to take an error handler and data.

--- src/rasqal_internal.h
+++ src/rasqal_internal.h
@@ -456,7 +456,7 @@ int rasqal_literal_as_boolean(rasqal_literal* literal, int* error);
 int rasqal_literal_as_integer(rasqal_literal* l, int* error);
 double rasqal_literal_as_floating(rasqal_literal* l, int* error);
 raptor_uri* rasqal_literal_as_uri(rasqal_literal* l);
-void rasqal_literal_string_to_native(rasqal_literal* l);
+int rasqal_literal_string_to_native(rasqal_literal *l, raptor_simple_message_handler error_handler, void *error_data);
 int rasqal_literal_has_qname(rasqal_literal* l);
 int rasqal_literal_expand_qname(void* user_data, rasqal_literal* l);
 int rasqal_expression_has_qname(void* user_data, rasqal_expression* e);
