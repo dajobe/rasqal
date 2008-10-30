2a708713d1a2a92464037b11fa753ce2e8655c3e
Dave Beckett <dave@dajobe.org>
2008-10-30 06:28:39 +0000

    (rasqal_algebra_project_algebra_node_to_rowsource): Added. (rasqal_algebra_node_to_rowsource): Use rasqal_algebra_project_algebra_node_to_rowsource to convert project algebra nodes.

--- src/rasqal_engine_algebra.c
+++ src/rasqal_engine_algebra.c
@@ -107,8 +107,8 @@ rasqal_algebra_filter_algebra_node_to_rowsource(rasqal_engine_algebra_data* exec
 
 static rasqal_rowsource*
 rasqal_algebra_orderby_algebra_node_to_rowsource(rasqal_engine_algebra_data* execution_data,
-                                                rasqal_algebra_node* node,
-                                                rasqal_engine_error *error_p)
+                                                 rasqal_algebra_node* node,
+                                                 rasqal_engine_error *error_p)
 {
   rasqal_query *query = execution_data->query;
   rasqal_rowsource *rs;
@@ -147,6 +147,23 @@ rasqal_algebra_union_algebra_node_to_rowsource(rasqal_engine_algebra_data* execu
 
 
 static rasqal_rowsource*
+rasqal_algebra_project_algebra_node_to_rowsource(rasqal_engine_algebra_data* execution_data,
+                                                 rasqal_algebra_node* node,
+                                                 rasqal_engine_error *error_p)
+{
+  rasqal_query *query = execution_data->query;
+  rasqal_rowsource *rs;
+
+  rs = rasqal_algebra_node_to_rowsource(execution_data, node->node1,
+                                        error_p);
+  if(!rs || *error_p)
+    return NULL;
+
+  return rasqal_new_project_rowsource(query, rs, node->vars_seq);
+}
+
+
+static rasqal_rowsource*
 rasqal_algebra_node_to_rowsource(rasqal_engine_algebra_data* execution_data,
                                  rasqal_algebra_node* node,
                                  rasqal_engine_error *error_p)
@@ -174,12 +191,16 @@ rasqal_algebra_node_to_rowsource(rasqal_engine_algebra_data* execution_data,
                                                           node, error_p);
       break;
 
+    case RASQAL_ALGEBRA_OPERATOR_PROJECT:
+      rs = rasqal_algebra_project_algebra_node_to_rowsource(execution_data,
+                                                            node, error_p);
+      break;
+
     case RASQAL_ALGEBRA_OPERATOR_UNKNOWN:
     case RASQAL_ALGEBRA_OPERATOR_JOIN:
     case RASQAL_ALGEBRA_OPERATOR_DIFF:
     case RASQAL_ALGEBRA_OPERATOR_LEFTJOIN:
     case RASQAL_ALGEBRA_OPERATOR_TOLIST:
-    case RASQAL_ALGEBRA_OPERATOR_PROJECT:
     case RASQAL_ALGEBRA_OPERATOR_DISTINCT:
     case RASQAL_ALGEBRA_OPERATOR_REDUCED:
     case RASQAL_ALGEBRA_OPERATOR_SLICE:
