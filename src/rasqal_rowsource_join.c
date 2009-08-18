c53d38e11662ebba287ef7c638977da653e2ece5
Dave Beckett <dave@dajobe.org>
2009-08-18 06:27:32 +0000

    Use rasqal_row_compatible to compute row compatibility. (rasqal_join_rowsource_init, rasqal_join_rowsource_finish): Init/free the compatible row map. (rasqal_join_rowsource_read_row): Simplify control flow in getting rows.  Check row compatibility always for all joins. For left joins, allow rows from left if condition or compatibility fails. (main): Alter test data so rows can be joined (!) and add left join and natural join tests.

--- src/rasqal_rowsource_join.c
+++ src/rasqal_rowsource_join.c
@@ -2,7 +2,7 @@
  *
  * rasqal_rowsource_join.c - Rasqal join rowsource class
  *
- * Copyright (C) 2008, David Beckett http://www.dajobe.org/
+ * Copyright (C) 2008-2009, David Beckett http://www.dajobe.org/
  * 
  * This package is Free Software and part of Redland http://librdf.org/
  * 
@@ -74,6 +74,9 @@ typedef struct
   
   /* join expression */
   rasqal_expression *expr;
+
+  /* map for checking compatibility of rows */
+  rasqal_row_compatible* rc_map;
 } rasqal_join_rowsource_context;
 
 
@@ -81,6 +84,8 @@ static int
 rasqal_join_rowsource_init(rasqal_rowsource* rowsource, void *user_data) 
 {
   rasqal_join_rowsource_context* con;
+  rasqal_variables_table* vars_table; 
+
   con = (rasqal_join_rowsource_context*)user_data;
 
   con->failed = 0;
@@ -139,6 +144,9 @@ rasqal_join_rowsource_init(rasqal_rowsource* rowsource, void *user_data)
   rasqal_rowsource_set_preserve(con->left, 1);
   rasqal_rowsource_set_preserve(con->right, 1);
   
+  vars_table = con->left->vars_table;
+  con->rc_map = rasqal_new_row_compatible(vars_table, con->left, con->right);
+
   return 0;
 }
 
@@ -159,6 +167,9 @@ rasqal_join_rowsource_finish(rasqal_rowsource* rowsource, void *user_data)
   
   if(con->expr)
     rasqal_free_expression(con->expr);
+  
+  if(con->rc_map)
+    rasqal_free_row_compatible(con->rc_map);
   
   RASQAL_FREE(rasqal_join_rowsource_context, con);
 
@@ -280,9 +291,9 @@ rasqal_join_rowsource_read_row(rasqal_rowsource* rowsource, void *user_data)
 
   while(1) {
     rasqal_row *right_row;
-    rasqal_literal *result;
-    int bresult;
-    
+    int bresult = 1;
+    int compatible = 1;
+
     if(con->state == INIT_RIGHT) {
       if(!con->left_row) {
         con->state = FINISHED;
@@ -290,20 +301,11 @@ rasqal_join_rowsource_read_row(rasqal_rowsource* rowsource, void *user_data)
       }
 
       rasqal_rowsource_reset(con->right);
-      right_row  = rasqal_rowsource_read_row(con->right);
-      row = rasqal_join_rowsource_build_merged_row(rowsource, con, right_row);
-      if(right_row) {
-        con->state = READ_RIGHT;
-      } else {
-        /* con->state = INIT_RIGHT; */
-        if(row)
-          con->left_row = rasqal_rowsource_read_row(con->left);
-      }
-      if(row)
-        goto have_row;
+      con->state = READ_RIGHT;
     }
 
-    /* else state is READ_RIGHT */
+
+    /* state is always READ_RIGHT at this point */
 
     right_row = rasqal_rowsource_read_row(con->right);
     if(!right_row) {
@@ -313,44 +315,55 @@ rasqal_join_rowsource_read_row(rasqal_rowsource* rowsource, void *user_data)
       continue;
     }
 
-    /* con->state = READ_RIGHT; */
-    row = rasqal_join_rowsource_build_merged_row(rowsource, con, right_row);
 
-    have_row:
-    if(!con->expr)
-      break;
-
-    result = rasqal_expression_evaluate_v2(rowsource->world, &query->locator,
-                                           con->expr, query->compare_flags);
+    /* now have both left and right rows so compute compatibility */
+    compatible = rasqal_row_compatible_check(con->rc_map,
+                                             con->left_row, right_row);
 #ifdef RASQAL_DEBUG
-    RASQAL_DEBUG1("join expression result:\n");
-    if(!result)
-      fputs("type error", DEBUG_FH);
-    else
-      rasqal_literal_print(result, DEBUG_FH);
-    fputc('\n', DEBUG_FH);
+    fprintf(stderr, "join rows compatible: %s\n", compatible ? "YES" : "NO");
 #endif
-    if(!result) {
-      bresult = 0;
-    } else {
-      int error = 0;
-      bresult = rasqal_literal_as_boolean(result, &error);
+
+
+    /* Check join expression if present */
+    if(con->expr) {
+      rasqal_literal *result;
+
+      result = rasqal_expression_evaluate_v2(rowsource->world, &query->locator,
+                                             con->expr, query->compare_flags);
 #ifdef RASQAL_DEBUG
-      if(error)
-        RASQAL_DEBUG1("filter boolean expression returned error\n");
+      RASQAL_DEBUG1("join expression result:\n");
+      if(!result)
+        fputs("type error", DEBUG_FH);
       else
-        RASQAL_DEBUG2("filter boolean expression result: %d\n", bresult);
+        rasqal_literal_print(result, DEBUG_FH);
+      fputc('\n', DEBUG_FH);
 #endif
-      rasqal_free_literal(result);
+      if(!result) {
+        bresult = 0;
+      } else {
+        int error = 0;
+        bresult = rasqal_literal_as_boolean(result, &error);
+#ifdef RASQAL_DEBUG
+        if(error)
+          RASQAL_DEBUG1("filter boolean expression returned error\n");
+        else
+          RASQAL_DEBUG2("filter boolean expression result: %d\n", bresult);
+#endif
+        rasqal_free_literal(result);
+      }
     }
-    if(bresult)
-      /* Constraint succeeded so return merged row */
-      break;
+    
+    if(bresult && compatible) {
+      /* No constraint OR constraint & compatible so return merged row */
 
+      /* Compute row only now it is known to be needed */
+      row = rasqal_join_rowsource_build_merged_row(rowsource, con, right_row);
+      break;
+    }
+    
     /* otherwise return LEFT or RIGHT row only */
-    rasqal_free_row(row); row = NULL;
     if(con->join_type == RASQAL_JOIN_TYPE_LEFT) {
-      /* LEFT JOIN - add left row if expr fails */
+      /* LEFT JOIN - add left row if expr fails or not compatible */
       if(con->left_row) {
         row = rasqal_join_rowsource_build_merged_row(rowsource, con, NULL);
         break;
@@ -503,11 +516,11 @@ const char* const join_1_data_2x3_rows[] =
   /* 2 variable names and 3 rows */
   "a",   NULL, "b",   NULL,
   /* row 1 data */
-  "foo", NULL, "bar", NULL,
+  "foo", NULL, "red", NULL,
   /* row 2 data */
-  "baz", NULL, "fez", NULL,
+  "baz", NULL, "blue", NULL,
   /* row 3 data */
-  "bob", NULL, "sue", NULL,
+  "bob", NULL, "green", NULL,
   /* end of data */
   NULL, NULL, NULL, NULL
 };
@@ -522,7 +535,7 @@ const char* const join_2_data_3x4_rows[] =
   /* row 2 data */
   "blue",  NULL, "indigo", NULL, "violet", NULL,
   /* row 3 data */
-  "black", NULL, "silver", NULL, "gold",   NULL,
+  "blue", NULL, "silver", NULL, "gold",   NULL,
   /* row 4 data */
   "green", NULL, "tope",   NULL, "bronze", NULL,
   /* end of data */
@@ -530,9 +543,20 @@ const char* const join_2_data_3x4_rows[] =
 };
 
 
-#define EXPECTED_ROWS_COUNT (3 * 4)
+/* join on b=red: 1 row b=blue: 2 rows and b=green: 1 row */
+typedef struct {
+  rasqal_join_type join_type;
+  int expected;
+} join_test_config_type;
 
-/* there is one duplicate variable 'b' */
+#define JOIN_TESTS_COUNT 2
+const join_test_config_type join_test_config[JOIN_TESTS_COUNT] = { 
+  { RASQAL_JOIN_TYPE_NATURAL, 4 },
+  { RASQAL_JOIN_TYPE_LEFT, 12 },
+};
+
+
+/* there is one variable 'b' that is joined on */
 #define EXPECTED_COLUMNS_COUNT (2 + 3 - 1)
 const char* const join_result_vars[] = { "a" , "b" , "c", "d" };
 
@@ -549,13 +573,12 @@ main(int argc, char *argv[])
   int count;
   raptor_sequence* seq = NULL;
   int failures = 0;
-  int vars_count;
   rasqal_variables_table* vt;
   int size;
-  int expected_count = EXPECTED_ROWS_COUNT;
   int expected_size = EXPECTED_COLUMNS_COUNT;
   int i;
   raptor_sequence* vars_seq = NULL;
+  int test_count;
   
   world = rasqal_new_world(); rasqal_world_open(world);
   
@@ -563,111 +586,124 @@ main(int argc, char *argv[])
   
   vt = query->vars_table;
 
-  /* 2 variables and 3 rows */
-  vars_count = 2;
-  seq = rasqal_new_row_sequence(world, vt, join_1_data_2x3_rows, vars_count,
-                                &vars_seq);
-  if(!seq) {
-    fprintf(stderr,
-            "%s: failed to create left sequence of %d vars\n", program,
-            vars_count);
-    failures++;
-    goto tidy;
-  }
+  for(test_count = 0; test_count < JOIN_TESTS_COUNT; test_count++) {
+    rasqal_join_type join_type = join_test_config[test_count].join_type;
+    int expected_count = join_test_config[test_count].expected;
+    int vars_count;
 
-  left_rs = rasqal_new_rowsequence_rowsource(world, query, vt, seq, vars_seq);
-  if(!left_rs) {
-    fprintf(stderr, "%s: failed to create left rowsource\n", program);
-    failures++;
-    goto tidy;
-  }
-  /* vars_seq and seq are now owned by left_rs */
-  vars_seq = seq = NULL;
-  
-  /* 3 variables and 4 rows */
-  vars_count = 3;
-  seq = rasqal_new_row_sequence(world, vt, join_2_data_3x4_rows, vars_count,
-                                &vars_seq);
-  if(!seq) {
-    fprintf(stderr,
-            "%s: failed to create right sequence of %d rows\n", program,
-            vars_count);
-    failures++;
-    goto tidy;
-  }
+    fprintf(stderr, "%s: test #%d  join type %d\n", program, test_count,
+            (int)join_type);
 
-  right_rs = rasqal_new_rowsequence_rowsource(world, query, vt, seq, vars_seq);
-  if(!right_rs) {
-    fprintf(stderr, "%s: failed to create right rowsource\n", program);
-    failures++;
-    goto tidy;
-  }
-  /* vars_seq and seq are now owned by right_rs */
-  vars_seq = seq = NULL;
-
-  rowsource = rasqal_new_join_rowsource(world, query, left_rs, right_rs,
-                                        RASQAL_JOIN_TYPE_LEFT, NULL);
-  if(!rowsource) {
-    fprintf(stderr, "%s: failed to create join rowsource\n", program);
-    failures++;
-    goto tidy;
-  }
-  /* left_rs and right_rs are now owned by rowsource */
-  left_rs = right_rs = NULL;
-
-  seq = rasqal_rowsource_read_all_rows(rowsource);
-  if(!seq) {
-    fprintf(stderr,
-            "%s: read_rows returned a NULL seq for a join rowsource\n",
-            program);
-    failures++;
-    goto tidy;
-  }
-  count = raptor_sequence_size(seq);
-  if(count != expected_count) {
-    fprintf(stderr,
-            "%s: read_rows returned %d rows for a join rowsource, expected %d\n",
-            program, count, expected_count);
-    failures++;
-    goto tidy;
-  }
-  
-  size = rasqal_rowsource_get_size(rowsource);
-  if(size != expected_size) {
-    fprintf(stderr,
-            "%s: read_rows returned %d columns (variables) for a join rowsource, expected %d\n",
-            program, size, expected_size);
-    failures++;
-    goto tidy;
-  }
-  for(i = 0; i < expected_size; i++) {
-    rasqal_variable* v;
-    const char* name = NULL;
-    const char *expected_name = join_result_vars[i];
-    
-    v = rasqal_rowsource_get_variable_by_offset(rowsource, i);
-    if(!v) {
+    /* 2 variables and 3 rows */
+    vars_count = 2;
+    seq = rasqal_new_row_sequence(world, vt, join_1_data_2x3_rows, vars_count,
+                                  &vars_seq);
+    if(!seq) {
       fprintf(stderr,
-            "%s: read_rows had NULL column (variable) #%d expected %s\n",
-              program, i, expected_name);
+              "%s: failed to create left sequence of %d vars\n", program,
+              vars_count);
       failures++;
       goto tidy;
     }
-    name = (const char*)v->name;
-    if(strcmp(name, expected_name)) {
-      fprintf(stderr,
-            "%s: read_rows returned column (variable) #%d %s but expected %s\n",
-              program, i, name, expected_name);
+
+    left_rs = rasqal_new_rowsequence_rowsource(world, query, vt, seq, vars_seq);
+    if(!left_rs) {
+      fprintf(stderr, "%s: failed to create left rowsource\n", program);
       failures++;
       goto tidy;
     }
-  }
-  
-  
+    /* vars_seq and seq are now owned by left_rs */
+    vars_seq = seq = NULL;
+
+    /* 3 variables and 4 rows */
+    vars_count = 3;
+    seq = rasqal_new_row_sequence(world, vt, join_2_data_3x4_rows, vars_count,
+                                  &vars_seq);
+    if(!seq) {
+      fprintf(stderr,
+              "%s: failed to create right sequence of %d rows\n", program,
+              vars_count);
+      failures++;
+      goto tidy;
+    }
+
+    right_rs = rasqal_new_rowsequence_rowsource(world, query, vt, seq, vars_seq);
+    if(!right_rs) {
+      fprintf(stderr, "%s: failed to create right rowsource\n", program);
+      failures++;
+      goto tidy;
+    }
+    /* vars_seq and seq are now owned by right_rs */
+    vars_seq = seq = NULL;
+
+    rowsource = rasqal_new_join_rowsource(world, query, left_rs, right_rs,
+                                          join_type, NULL);
+    if(!rowsource) {
+      fprintf(stderr, "%s: failed to create join rowsource\n", program);
+      failures++;
+      goto tidy;
+    }
+    /* left_rs and right_rs are now owned by rowsource */
+    left_rs = right_rs = NULL;
+
+    seq = rasqal_rowsource_read_all_rows(rowsource);
+    if(!seq) {
+      fprintf(stderr,
+              "%s: read_rows returned a NULL seq for a join rowsource\n",
+              program);
+      failures++;
+      goto tidy;
+    }
+    count = raptor_sequence_size(seq);
+    if(count != expected_count) {
+      fprintf(stderr,
+              "%s: read_rows returned %d rows for a join rowsource, expected %d\n",
+              program, count, expected_count);
+      failures++;
+      goto tidy;
+    }
+
+    size = rasqal_rowsource_get_size(rowsource);
+    if(size != expected_size) {
+      fprintf(stderr,
+              "%s: read_rows returned %d columns (variables) for a join rowsource, expected %d\n",
+              program, size, expected_size);
+      failures++;
+      goto tidy;
+    }
+    for(i = 0; i < expected_size; i++) {
+      rasqal_variable* v;
+      const char* name = NULL;
+      const char *expected_name = join_result_vars[i];
+
+      v = rasqal_rowsource_get_variable_by_offset(rowsource, i);
+      if(!v) {
+        fprintf(stderr,
+              "%s: read_rows had NULL column (variable) #%d expected %s\n",
+                program, i, expected_name);
+        failures++;
+        goto tidy;
+      }
+      name = (const char*)v->name;
+      if(strcmp(name, expected_name)) {
+        fprintf(stderr,
+              "%s: read_rows returned column (variable) #%d %s but expected %s\n",
+                program, i, name, expected_name);
+        failures++;
+        goto tidy;
+      }
+    }
+
 #ifdef RASQAL_DEBUG
-  rasqal_rowsource_print_row_sequence(rowsource, seq, DEBUG_FH);
+    rasqal_rowsource_print_row_sequence(rowsource, seq, DEBUG_FH);
 #endif
 
+    raptor_free_sequence(seq); seq = NULL;
+    rasqal_free_rowsource(rowsource); rowsource = NULL;
+    
+    /* end test_count loop */
+  }
+  
   tidy:
   if(seq)
     raptor_free_sequence(seq);
