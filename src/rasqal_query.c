b32476459a582be054c2e7b9f1ea22bfd7abfd06
Dave Beckett <dave@dajobe.org>
2009-09-02 18:46:44 +0000

    (rasqal_query_add_prefix): Handle adding/updating with the default namespace - NULL prefix string

--- src/rasqal_query.c
+++ src/rasqal_query.c
@@ -984,10 +984,14 @@ rasqal_query_add_prefix(rasqal_query* query, rasqal_prefix* prefix)
       return 1;
   } else {
     int i;
-    for(i = 0; i< raptor_sequence_size(query->prefixes); i++) {
+    for(i = 0; i < raptor_sequence_size(query->prefixes); i++) {
       rasqal_prefix* p;
       p = (rasqal_prefix*)raptor_sequence_get_at(query->prefixes, i);
-      if(strcmp((const char*)p->prefix, (const char*)prefix->prefix)) {
+
+      if((!p->prefix && !prefix->prefix) ||
+         ((p->prefix && prefix->prefix &&
+          !strcmp((const char*)p->prefix, (const char*)prefix->prefix)))
+        ) {
         rasqal_query_undeclare_prefix(query, p);
         break;
       }
