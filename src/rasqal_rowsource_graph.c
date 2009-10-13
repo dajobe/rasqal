8ea464aea875af35c16bfacf75e7123133cddc98
Dave Beckett <dave@dajobe.org>
2009-10-13 07:17:50 +0000

    (rasqal_graph_rowsource_init): Use rasqal_graph_rowsource_reset to set up initial graph state. (rasqal_graph_rowsource_reset): Call rasqal_graph_next_dg to set graph variable and start state.

--- src/rasqal_rowsource_graph.c
+++ src/rasqal_rowsource_graph.c
@@ -89,6 +89,9 @@ typedef struct
 } rasqal_graph_rowsource_context;
 
 
+static int rasqal_graph_rowsource_reset(rasqal_rowsource* rowsource, void *user_data);
+
+
 static int
 rasqal_graph_next_dg(rasqal_graph_rowsource_context *con) 
 {
@@ -135,15 +138,7 @@ rasqal_graph_next_dg(rasqal_graph_rowsource_context *con)
 static int
 rasqal_graph_rowsource_init(rasqal_rowsource* rowsource, void *user_data)
 {
-  rasqal_graph_rowsource_context *con;
-
-  con = (rasqal_graph_rowsource_context*)user_data;
-
-  con->offset = 0;
-  con->dg_offset = -1;
-  con->finished = 0;
-
-  return rasqal_graph_next_dg(con);
+  return rasqal_graph_rowsource_reset(rowsource, user_data);
 }
 
 
@@ -241,8 +236,11 @@ rasqal_graph_rowsource_reset(rasqal_rowsource* rowsource, void *user_data)
   rasqal_graph_rowsource_context *con;
   con = (rasqal_graph_rowsource_context*)user_data;
 
+  con->finished = 0;
   con->dg_offset = -1;
   con->offset = 0;
+
+  rasqal_graph_next_dg(con);
   
   return rasqal_rowsource_reset(con->rowsource);
 }
