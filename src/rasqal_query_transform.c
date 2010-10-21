8637ac57a8281a58e28931a49b421b7b9618d426
Lauri Aalto <laalto@iki.fi>
2010-10-21 15:13:03 +0300

    #ws

--- src/rasqal_query_transform.c
+++ src/rasqal_query_transform.c
@@ -180,8 +180,8 @@ rasqal_query_convert_blank_node_to_anonymous_variable(rasqal_query *rq,
   rasqal_variable* v;
   
   v = rasqal_variables_table_add(rq->vars_table,
-                                RASQAL_VARIABLE_TYPE_ANONYMOUS,
-                                (unsigned char*)l->string, NULL);
+                                 RASQAL_VARIABLE_TYPE_ANONYMOUS,
+                                 (unsigned char*)l->string, NULL);
   /* rasqal_new_variable_typed took ownership of the l->string name.
    * Set to NULL to prevent double delete. */
   l->string = NULL;
