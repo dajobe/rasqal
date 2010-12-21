74a8798520ab0ef104c9709074cceebd98f75c94
Dave Beckett <dave@dajobe.org>
2010-12-20 21:59:53 -0800

    Fix XSD decimal formatting to match canonical lexical format.
    (rasqal_xsd_decimal_as_string): Use appropriate library snprintf and
    then prune trailing 0s

--- src/rasqal_decimal.c
+++ src/rasqal_decimal.c
@@ -319,6 +319,24 @@ rasqal_xsd_decimal_get_double(rasqal_xsd_decimal* dec)
 }
 
 
+#ifdef RASQAL_DECIMAL_C99
+#define DECIMAL_SNPRINTF snprintf
+#define DECIMAL_FMT "%DDf"
+#endif
+#ifdef RASQAL_DECIMAL_MPFR
+#define DECIMAL_SNPRINTF mpfr_snprintf
+#define DECIMAL_FMT "%%.%dRf"
+#endif
+#ifdef RASQAL_DECIMAL_GMP
+#define DECIMAL_SNPRINTF gmp_snprintf
+#define DECIMAL_FMT "%%.%dFf"
+#endif
+#ifdef RASQAL_DECIMAL_NONE
+#define DECIMAL_SNPRINTF snprintf
+#define DECIMAL_FMT "%%.%df"
+#endif
+
+
 /**
  * rasqal_xsd_decimal_as_string:
  * @dec: XSD Decimal
@@ -335,177 +353,63 @@ rasqal_xsd_decimal_as_string(rasqal_xsd_decimal* dec)
 {
   char *s=NULL;
   size_t len=0;
-#if defined(RASQAL_DECIMAL_MPFR) || defined(RASQAL_DECIMAL_GMP)
-  mp_exp_t expo;
-  char *mpf_s;
+#ifdef RASQAL_DECIMAL_C99
+#else
+  char fmt[16];
+  size_t e_index = 0;
+  size_t trailing_zero_start = 0;
 #endif
   
   if(dec->string)
     return dec->string;
   
 #ifdef RASQAL_DECIMAL_C99
-  len=dec->precision_digits;
-  s=RASQAL_MALLOC(cstring, len+1);
+  len = dec->precision_digits;
+  s = RASQAL_MALLOC(cstring, len + 1);
   if(!s)
     return NULL;
+
   /* NOTE: Never seen a sprintf that supports _Decimal yet */
   snprintf(s, len, "%DDf", dec->raw);
-  len=strlen(s);
-#endif
-#ifdef RASQAL_DECIMAL_MPFR
-  mpf_s = mpfr_get_str(NULL, &expo, 10, dec->precision_digits, dec->raw,
-                       dec->rounding);
-  if(mpf_s) {
-    size_t from_len=strlen(mpf_s);
-    char *from_p=mpf_s;
-    size_t to_len;
-
-    /* 5=strlen("0.0") + 1 for sign */
-    to_len = !from_len ? 4 : (from_len * 2);
-
-    s = (char*)RASQAL_MALLOC(cstring, to_len + 1);
-    if(!s) {
-      mpfr_free_str((char*)mpf_s);
-      return NULL;
-    }
-    /* first digit of mantissa */
-    if(!*from_p || *from_p == '0') {
-      len = 3;
-      memcpy(s, "0.0", len + 1);
-    } else {
-      char* to_p=s;
-      int n;
-      
-      if(*from_p == '-') {
-        *to_p++ = *from_p++;
-        from_len--;
-      }
-
-      *to_p++ = *from_p++;
-      from_len--;
-      *to_p++ = '.';
-
-      /* rest of mantissa */
-      /* remove trailing 0s */
-      while(from_len > 1 && from_p[from_len-1]=='0')
-        from_len--;
-      memcpy(to_p, from_p, from_len);
-      to_p += from_len;
-
-      /* exp */
-      if(expo != 1) {
-        n = sprintf(to_p, "e%ld", expo-1);
-        to_p += n;
-      }
-
-      len = to_p - s;
-    }
-    mpfr_free_str((char*)mpf_s);
-  }
-#endif
-#ifdef RASQAL_DECIMAL_GMP
-  mpf_s=mpf_get_str(NULL, &expo, 10, dec->precision_digits, dec->raw);
-  if(mpf_s) {
-    size_t from_len=strlen(mpf_s);
-    char *from_p=mpf_s;
-    size_t to_len;
-
-    /* 5 = strlen("0.0") + 1 for sign */
-    to_len = !from_len ? 5 : (from_len * 2);
-
-    s = RASQAL_MALLOC(cstring, to_len + 1);
-    if(!s) {
-      free(mpf_s);
-      return NULL;
-    }
-    /* first digit of mantissa */
-    if(!*from_p || *from_p == '0') {
-      len = 3;
-      memcpy(s, "0.0", len + 1);
-    } else {
-      char *to_p=s;
-      int n;
-      
-      if(*from_p == '-') {
-        *to_p++ = *from_p++;
-        from_len--;
-      }
-
-      *to_p++ = *from_p++;
-      from_len--;
-      *to_p++ = '.';
-      /* rest of mantissa */
-      memcpy(to_p, from_p, from_len);
-      to_p+= from_len;
-
-      /* exp */
-      if(expo != 1) {
-        n = sprintf(to_p, "e%ld", expo-1);
-        to_p += n;
-      }
-
-      len = to_p - s;
-    }
-    free(mpf_s);
-  }
-#endif
-#ifdef RASQAL_DECIMAL_NONE
-  {
-    char *p, *p2;
-    char fmt[16];
-    /* construct a format string */
-    snprintf(fmt, sizeof(fmt), "%%.%de", dec->precision_digits);
+  len = strlen(s);
+#else
+  /* construct a format string */
+  snprintf(fmt, sizeof(fmt), DECIMAL_FMT, dec->precision_digits);
     
-    /* snprintf with no buffer to get buffer length */
-    len=snprintf(NULL, 0, fmt, dec->raw)+1;
-    s=(char*)RASQAL_MALLOC(cstring, len);
-    if(!s)
-      return NULL;
+  /* decimal snprintf with no buffer to get buffer length */
+  len = DECIMAL_SNPRINTF(NULL, 0, fmt, dec->raw);
 
-    /* format with snprintf */
-    snprintf(s, len, fmt, dec->raw);
+  s = (char*)RASQAL_MALLOC(cstring, len + 1);
+  if(!s)
+    return NULL;
+  
+  /* format into allocated buffer */
+  DECIMAL_SNPRINTF(s, len, fmt, dec->raw);
 
-    /* "1.2000e+02"
-     * - remove zeros before 'e', leave one for ".0"
-     * - remove '+' after 'e' (leave '-')
-     * - remove leading zero after 'e' */
+  len = strlen(s);
 
-    /* find 'e' */
-    p=strchr(s, 'e');
-
-    /* move 'e' and everything that follows on top of the trailing zeros */
-    for(p2=p; *--p2 == '0'; ) ;
-    if(*p2=='.') p2++; /* leave ".0" */
-    p2++;
-    
-    /* move string tail if required */
-    if(p != p2)
-      while((*p2++=*p++)) ;
-
-    /* adjust p to the first char after 'e' */
-    p=strchr(s, 'e');
-    p++;
-
-    /* leave '-' but remove '+' */
-    if(*p=='-') {
-      p++;
-      p2=p;
-    } else {
-      p2=p+1;
-    }
-    /* skip leading zero afer "e[+-]" */
-    if(*p2=='0') p2++;
-
-    /* move string tail if required */
-    if(p != p2)
-      while((*p++=*p2++)) ;
-
-    len=strlen(s);
+  /* find trailing zeros */
+  for(e_index = len - 1;
+      e_index > 0 && s[e_index] == '0';
+      e_index--) {
+    trailing_zero_start = e_index;
   }
+
+  /* Stop string where the trailing zeros started */
+  if(trailing_zero_start) {
+    s[trailing_zero_start] = '\0';
+    len = trailing_zero_start;
+  }
+  
+  if(s[len - 1] == '.') {
+    s[len-- - 1] = '\0';
+  }
+  
 #endif
 
-  dec->string=s;
-  dec->string_len=len;
+  dec->string = s;
+  dec->string_len = len;
+
   return s;
 }
 
@@ -830,13 +734,13 @@ main(int argc, char *argv[]) {
   double result_d;
   char *result_s;
   int result_i;
-  const long a_long=1234567890L;
-  const double a_double=1234567890e0;
-  const char* b_string="123456789012345678e0";
-  const char* expected_a_plus_b="1.23456790246913568e17";
-  const char* expected_a_plus_b_minus_b="1.23456789e9";
-  const char* expected_a_plus_b_minus_b_minus_a="0.0e0";
-  const char* expected_negative_b="-1.23456789012345678e17";
+  const long a_long = 1234567890L;
+  const double a_double = 1234567890e0;
+  const char* b_string = "123456789012345678";
+  const char* expected_a_plus_b = "123456790246913568";
+  const char* expected_a_plus_b_minus_b = "1234567890";
+  const char* expected_a_plus_b_minus_b_minus_a = "0";
+  const char* expected_negative_b = "-123456789012345678";
   int expected_a_compare_b= -1;
   int expected_a_equals_b= 0;
   rasqal_world *world;
