d831805481673741cf84dc53256d4ce20a705345
Dave Beckett <dave@dajobe.org>
2010-12-30 18:25:31 -0800

    Remove regex headers

--- src/rasqal_expr_evaluate.c
+++ src/rasqal_expr_evaluate.c
@@ -38,15 +38,6 @@
 #endif
 #include <stdarg.h>
 
-#ifdef RASQAL_REGEX_PCRE
-#include <pcre.h>
-#endif
-
-#ifdef RASQAL_REGEX_POSIX
-#include <sys/types.h>
-#include <regex.h>
-#endif
-
 #include "rasqal.h"
 #include "rasqal_internal.h"
 
