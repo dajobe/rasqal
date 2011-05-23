27a9af317cd2b122628138181b9df6c8a5098e23
Dave Beckett <dave@dajobe.org>
2011-05-22 21:05:24 -0700

    Fixes [CLANG]
    (rasqal_random_get_system_seed): Removed un-necessary final variable
    calculations and assignments that never get used

--- src/rasqal_random.c
+++ src/rasqal_random.c
@@ -66,8 +66,8 @@ rasqal_random_get_system_seed(rasqal_world* world)
   b -= a;  b ^= rot(a, 6);  a += c;
   c -= b;  c ^= rot(b, 8);  b += a;
   a -= c;  a ^= rot(c,16);  c += b;
-  b -= a;  b ^= rot(a,19);  a += c;
-  c -= b;  c ^= rot(b, 4);  b += a;
+  b -= a;  b ^= rot(a,19);  /* a += c; */ /* CLANG: not needed because of below */
+  c -= b;  c ^= rot(b, 4);  /* b += a; */ /* CLANG: last calculation not needed */
   
   return (unsigned int)c;
 }
