a5a439d125e31f70920b79205b5a121b8ae8b1d0
Dave Beckett <dave@dajobe.org>
2011-06-05 21:58:21 -0700

    warning txt

--- src/rasqal_service.c
+++ src/rasqal_service.c
@@ -366,7 +366,7 @@ rasqal_service_execute(rasqal_service* svc)
           if(bg_graph_count == 2) {
             /* Warn once, only when the second BG is seen */
             rasqal_log_error_simple(svc->world, RAPTOR_LOG_LEVEL_WARN, NULL,
-                                    "Attempted to add use background graphs");
+                                    "Attempted to add multiple background graphs");
           }
           /* always skip after first BG */
           continue;
