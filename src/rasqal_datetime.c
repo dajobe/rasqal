eea29f98276b1cc568e8eea941ca4b720a00e268
Dave Beckett <dave@dajobe.org>
2011-11-16 23:51:27 -0800

    use TIME_WITH_SYS_TIME

--- src/rasqal_datetime.c
+++ src/rasqal_datetime.c
@@ -34,8 +34,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#ifdef HAVE_SYS_TIME_H
-#include <sys/time.h>
+#ifdef TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# ifdef HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
 #endif
 #ifdef HAVE_STDLIB_H
 #include <stdlib.h>
