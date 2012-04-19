599101552a0516eb1d97fcd955a98a1cd89c3472
Dave Beckett <dave@dajobe.org>
2012-04-18 22:18:19 -0700

    (rasqal_algebra_new_true_expr): Added and use it

--- src/rasqal_algebra.c
+++ src/rasqal_algebra.c
@@ -1090,6 +1090,21 @@ rasqal_algebra_union_graph_pattern_to_algebra(rasqal_query* query,
 }
 
 
+static rasqal_expression*
+rasqal_algebra_new_true_expr(rasqal_query* query) 
+{
+  rasqal_literal *true_lit;
+  
+  true_lit = rasqal_new_boolean_literal(query->world, 1);
+  if(!true_lit) {
+    RASQAL_DEBUG1("rasqal_new_boolean_literal() failed\n");
+    return NULL;
+  }
+  
+  return rasqal_new_literal_expression(query->world, true_lit);
+}
+
+
 static rasqal_algebra_node*
 rasqal_algebra_group_graph_pattern_to_algebra(rasqal_query* query,
                                               rasqal_graph_pattern* gp)
@@ -1167,24 +1182,14 @@ rasqal_algebra_group_graph_pattern_to_algebra(rasqal_query* query,
             goto fail;
           }
         } else  {
-          rasqal_literal *true_lit = NULL;
           rasqal_expression *true_expr = NULL;
 
-          true_lit = rasqal_new_boolean_literal(query->world, 1);
-          if(!true_lit) {
-            RASQAL_DEBUG1("rasqal_new_boolean_literal() failed\n");
+          true_expr = rasqal_algebra_new_true_expr(query);
+          if(!true_expr) {
             rasqal_free_algebra_node(anode);
-            goto fail;
+            return NULL;
           }
           
-          true_expr = rasqal_new_literal_expression(query->world, true_lit);
-          if(!true_expr) {
-            RASQAL_DEBUG1("rasqal_new_literal_expression() failed\n");
-            rasqal_free_algebra_node(anode);
-            goto fail;
-          }
-          true_lit = NULL; /* now owned by true_expr */
-
           /* G := LeftJoin(G, A, true) */
           gnode = rasqal_new_leftjoin_algebra_node(query, gnode, anode,
                                                    true_expr);
