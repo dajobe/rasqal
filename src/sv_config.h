acc1673a0f5e12f361a6a84c40688092cc5170be
Dave Beckett <dave@dajobe.org>
2013-11-26 21:34:57 -0800

    redefine sv_set_option

--- src/sv_config.h
+++ src/sv_config.h
@@ -8,6 +8,7 @@
 
 #define sv_init rasqal_sv_init
 #define sv_free rasqal_sv_free
+#define sv_set_option rasqal_sv_set_option
 #define sv_get_line rasqal_sv_get_line
 #define sv_get_header rasqal_sv_get_header
 #define sv_parse_chunk rasqal_sv_parse_chunk
