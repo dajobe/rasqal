700be4e7c5a92a1b9495d8681e121350a605f839
Dave Beckett <dave@dajobe.org>
2014-06-02 11:10:17 -0700

    (rasqal_literal_as_uri): Do not lookup NULL variable value

--- src/rasqal_literal.c
+++ src/rasqal_literal.c
@@ -1709,10 +1709,11 @@ rasqal_literal_as_uri(rasqal_literal* l)
   if(l->type == RASQAL_LITERAL_URI)
     return l->value.uri;
 
-  if(l->type == RASQAL_LITERAL_VARIABLE)
+  if(l->type == RASQAL_LITERAL_VARIABLE && l->value.variable->value)
     return rasqal_literal_as_uri(l->value.variable->value);
 
-  RASQAL_FATAL2("Literal type %d has no URI value", l->type);
+  RASQAL_FATAL3("Literal type %s (%d) has no URI value", 
+                rasqal_literal_type_label(l->type), l->type);
 
   return NULL;
 }
