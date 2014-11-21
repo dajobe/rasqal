438e54214a79a6d8709ca66f87327a9a795ae50d
Dave Beckett <dave@dajobe.org>
2014-11-21 13:49:31 -0800

    Tidy code

--- scripts/install-raptor.sh
+++ scripts/install-raptor.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-# Helper script to install a particula raptor version for travis CI
+# Helper script to install a particular raptor version
+#
+# Intended for use with travis CI; see .travis.yml at the root
 
 set -x
 
@@ -7,14 +9,19 @@ PACKAGE=raptor2
 MIN_VERSION=$1
 INSTALL_VERSION=$2
 
+FILE="$PACKAGE-$INSTALL_VERSION.tar.gz"
+URL="http://download.librdf.org/source/$FILE"
+
+ROOT_DIR=${TMPDIR:-/tmp}
+BUILD_DIR="$ROOT_DIR/build-$PACKAGE"
+
 raptor_version=`pkg-config --modversion $PACKAGE`
 if pkg-config --atleast-version $MIN_VERSION $PACKAGE; then
-  echo "Raptor2 $raptor_version is new enough"
+    echo "Raptor2 $raptor_version is new enough"
 else
-  cd /tmp
-  wget http://download.librdf.org/source/$PACKAGE-$INSTALL_VERSION.tar.gz
-  tar -x -z -f $PACKAGE-$INSTALL_VERSION.tar.gz
-  cd $PACKAGE-$INSTALL_VERSION && ./configure --prefix=/usr && make && sudo make install
-  cd ..
-  rm -rf raptor-$INSTALL_VERSION
+    mkdir $BUILD_DIR && cd $BUILD_DIR
+    wget -O $FILE $URL || curl -o $FILE $URL
+    tar -x -z -f $FILE && rm $FILE
+    cd $PACKAGE-$INSTALL_VERSION && ./configure --prefix=/usr && make && sudo make install
+    cd / && rm -rf $BUILD_DIR
 fi
